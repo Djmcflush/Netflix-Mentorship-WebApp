cee430ff1846493fad6c84544af0d69d0f140046
D.Edwards <dae3f@uvawise.edu>
2020-03-19 15:43:56 -0400

    Add files via upload

--- app.py
+++ app.py
@@ -2,13 +2,32 @@ import numpy as np
 from flask import Flask, request, jsonify, render_template
 import pickle
 from Freq_Analysis import Freq
-
+from sklearn.preprocessing import StandardScaler 
+import pandas as pd
 app = Flask(__name__)
 model = pickle.load(open('Finalmodel.pkl', 'rb'))
 
 @app.route('/')
 def home():
     return render_template('index.html')
+def create_scalers():
+    df = pd.read_csv('finaldf.csv')
+    df = df.drop('Unnamed: 0', axis =1)
+
+    fdf = pd.read_csv('feature.csv')
+    fdf = fdf.drop('Unnamed: 0', axis =1)
+
+    target_pd = pd.DataFrame(df['Target'])
+    df = df.drop('Target', axis=1)
+
+    scaler = StandardScaler() 
+    target_scaler = StandardScaler()
+    # To scale data 
+    
+    scaler.fit(fdf) 
+    target_scaler.fit(target_pd)
+    return scaler, target_scaler
+    
 
 @app.route('/predict',methods=['POST'])
 def predict():
@@ -30,16 +49,27 @@ def predict():
         common_words+= x
     rare_words = rare_words/5
     common_words = common_words/5
-    all_words = sum(tokenized_description)/len(tokenized_description)
+    all_words = sum(tokenized_description) /len(tokenized_description)
     features.append(all_words)
     features.append(rare_words)
     features.append(common_words)
+    print(common_words, 'This is the common english score')
     final_features = [np.array(features)]
+    
+    # And now to load..
+    scaler, target_scaler = create_scalers()
+
+    
+    final_features = scaler.transform(final_features)
 
     prediction = model.predict(final_features)
-
+    
     output = round(prediction[0], 2)
-
+    output = target_scaler.inverse_transform([output,.7])
+    output = output[0]
+    output = round(output,2)
+    output = '{:,}'.format(output) 
+    
     return render_template('index.html', prediction_text='Overseas Revenue for this film should be $ {}'.format(output))
 
 @app.route('/results',methods=['POST'])
